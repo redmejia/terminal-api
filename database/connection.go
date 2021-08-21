dde69fb8212e3c6340f9ab52bc301669d8125986
redmejia <preynaldorod@gmail.com>
2021-08-20 21:01:03 -0700

    creating pool connnection

--- database/connection.go
+++ database/connection.go
@@ -5,22 +5,35 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-func Conn() (*sql.DB, error) {
+// hold connection
+var DB *sql.DB
 
+const (
+	openConns = 10
+	idleConns = 3
+	lifeTime  = 5 * time.Minute
+)
+
+func Conn() (*sql.DB, error) {
 	port, _ := strconv.Atoi(os.Getenv("DBPORT"))
 	connDB := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
 		os.Getenv("HOSTNAME"), port, os.Getenv("DBUSER"), os.Getenv("DBPASSWORD"),
 		os.Getenv("DBNAME"), os.Getenv("DBSSLMODE"),
 	)
 
-	db, err := sql.Open("pgx", connDB)
+	DB, err := sql.Open("pgx", connDB)
 	if err != nil {
 		return nil, err
 	}
 
-	return db, nil
+	DB.SetMaxOpenConns(openConns)
+	DB.SetMaxIdleConns(idleConns)
+	DB.SetConnMaxLifetime(lifeTime)
+
+	return DB, nil
 }
