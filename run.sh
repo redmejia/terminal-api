718180d68886785215effd8e33ff095860ff93ca
redmejia <preynaldorod@gmail.com>
2021-08-26 13:55:42 -0700

    run file

--- run.sh
+++ run.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+# clear the term
+clear
+
 echo "terminal"
 
+# datatase enviroment variables
 DBENV=.dbenv
 
+# check if file exist
 if [ -e "$DBENV" ]; then
 	echo "$DBENV setting database enviroments"
 	. .dbenv
 	echo "run server"
 	go run *.*go
 else
-	echo "...NOT FOUND"
-	echo "create database enviroment variable file"
+	echo "$DBENV ...NOT FOUND"
+	echo "...creating"
+	touch .dbenv
+	echo "file was creates edit with enviroment vars"
 fi
