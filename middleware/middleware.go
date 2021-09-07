1ab705739c4fd3ae9e736a5adfa8591cf134960b
redmejia <preynaldorod@gmail.com>
2021-09-07 15:56:01 -0700

    comments

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,10 +2,12 @@ package middleware
 
 import "log"
 
+// Middleware
 type Middleware struct {
 	InfoLog, ErrorLog *log.Logger
 }
 
+// NewMiddleware
 func NewMiddleware(infoLog, errLog *log.Logger) *Middleware {
 	return &Middleware{
 		InfoLog:  infoLog,
