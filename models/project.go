70b50174d2311fee5f71bc0313c5fd6e831a89b3
redmejia <preynaldorod@gmail.com>
2021-10-31 01:01:00 -0700

    fix time problem format time

--- models/project.go
+++ models/project.go
@@ -6,7 +6,7 @@ type Project struct {
 	DevID              int64  `json:"dev_id"`
 	ProjectName        string `json:"project_name"`
 	ProjectDescription string `json:"project_description"`
-	Created            int64  `json:"created"` // time.Time
+	Created            string `json:"created"` // time.Time
 	CreatedBy          string `json:"created_by"`
 	ProjectRepo        string `json:"project_repo"`
 	ProjectLive        string `json:"project_live"`
